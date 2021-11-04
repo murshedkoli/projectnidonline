092e03507f6d0b8d81554837b3d7e6e99f0fbc53
murshedkoli <murshedkoli@gmail.com>
2021-11-04 18:48:26 +0600

    first update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
-import logo from './logo.svg';
 import './App.css';
-
+import r1 from './images/ri_1.jpeg'
+import r2 from './images/ri_2.jpeg'
+import r3 from './images/ri_3.jpeg'
+import r4 from './images/ri_4.png'
+import r5 from './images/ri_5.png'
+import r6 from './images/ri_6.png'
+import v1 from './images/vi_1.png'
+import v2 from './images/vi_2.png'
+import v3 from './images/vi_3.png'
+import v4 from './images/vi_4.png'
+import v5 from './images/vi_5.png'
+import v6 from './images/vi_6.png'
+import v7 from './images/vi_7.png'
+import v8 from './images/vi_8.png'
+import v9 from './images/vi_9.png'
+import v10 from './images/vi_10.png'
+import v11 from './images/vi_11.png'
+import v12 from './images/vi_12.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+
+      <div>
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <img style={{ position: 'absolute', top: '0.83in', left: '0.83in', width: '3.67in', height: '0.02in' }} src={v1} />
+        <img style={{ position: 'absolute', top: '0.83in', left: '4.49in', width: '0.02in', height: '2.33in' }} src={v2} />
+        <img style={{ position: 'absolute', top: '3.15in', left: '0.83in', width: '3.67in', height: '0.02in' }} src={v3} />
+        <img style={{ position: 'absolute', top: '0.83in', left: '0.83in', width: '0.02in', height: '2.33in' }} src={v4} />
+        <img style={{ position: 'absolute', top: '0.94in', left: '0.90in', width: '0.42in', height: '0.41in' }} src={r1} />
+        <div style={{ position: 'absolute', top: '0.90in', left: '1.86in', width: '2.18in', lineHeight: '0.26in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '14pt', fontFamily: 'Bangla', color: '#000000' }}>গণপ্রজাতন্ত্রী
+          বাংলােদশ সরকার</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '15pt', fontFamily: 'Bangla', color: '#000000' }}>
+          </span><br /></div>
+        <div style={{ position: 'absolute', top: '1.16in', left: '1.40in', width: '3.06in', lineHeight: '0.16in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'ArialMT', color: '#007700' }}>Government of
+          the People's Republic of Bangladesh</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'ArialMT', color: '#007700' }}> </span><br />
+          <div style={{ position: 'relative', left: '0.59in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '11pt', fontFamily: 'ArialMT', color: '#ff0000' }}>National
+            ID
+            Card</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'ArialMT', color: '#000000' }}> /
+            </span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '11pt', fontFamily: 'Bangla', color: '#000000' }}>জাতীয় পিরচয়
+              পত্র</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>
+            </span><br /></div>
+        </div>
+        <img style={{ position: 'absolute', top: '1.59in', left: '0.88in', width: '0.77in', height: '0.88in' }} src={r2} />
+        <img style={{ position: 'absolute', top: '2.53in', left: '0.88in', width: '0.77in', height: '0.38in' }} src={r3} />
+        <img style={{ position: 'absolute', top: '1.56in', left: '1.68in', width: '2.81in', height: '1.54in' }} src={r4} />
+        <div style={{ position: 'absolute', top: '1.61in', left: '1.76in', width: '0.32in', lineHeight: '0.21in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '12pt', fontFamily: 'Bangla', color: '#000000' }}>নাম:</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '12pt', fontFamily: 'Bangla', color: '#000000' }}>
+        </span><br /></div>
+        <div style={{ position: 'absolute', top: '1.61in', left: '2.23in', width: '0.67in', lineHeight: '0.21in' }}><span style={{ fontStyle: 'normal', fontWeight: 'bold', fontSize: '12pt', fontFamily: 'Bangla', color: '#000000' }}>সুজন মিয়া
+        </span><br /></div>
+        <div style={{ position: 'absolute', top: '1.61in', left: '2.23in', width: '0.67in', lineHeight: '0.21in' }}>
+        </div>
+        <div style={{ position: 'absolute', top: '1.93in', left: '1.76in', width: '0.37in', lineHeight: '0.15in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'ArialMT', color: '#000000' }}>Name:</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '7pt', fontFamily: 'ArialMT', color: '#000000' }}>
+        </span><br /></div>
+        <div style={{ position: 'absolute', top: '1.91in', left: '2.23in', width: '0.82in', lineHeight: '0.21in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'ArialMT', color: '#000000' }}>Shojon
+          Mia</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'ArialMT', color: '#000000' }}>
+          </span><br /></div>
+        <div style={{ position: 'absolute', top: '2.16in', left: '1.76in', width: '0.32in', lineHeight: '0.18in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>পিতা:</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>
+        </span><br /></div>
+        <div style={{ position: 'absolute', top: '2.16in', left: '2.23in', width: '0.74in', lineHeight: '0.18in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>চাঁন মিয়া
+        </span><br /></div>
+        <div style={{ position: 'absolute', top: '2.16in', left: '2.23in', width: '0.74in', lineHeight: '0.18in' }}>
+        </div>
+        <div style={{ position: 'absolute', top: '2.43in', left: '1.76in', width: '0.32in', lineHeight: '0.18in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>মাতা:</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>
+        </span><br /></div>
+        <div style={{ position: 'absolute', top: '2.43in', left: '2.23in', width: '0.66in', lineHeight: '0.18in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>রুপিয়া
+          বেগম</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>
+          </span><br /></div>
+        <div style={{ position: 'absolute', top: '2.70in', left: '1.77in', width: '1.62in', lineHeight: '0.19in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'ArialMT', color: '#000000' }}>Date of Birth:
+        </span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '11pt', fontFamily: 'ArialMT', color: '#ff0000' }}>&nbsp;01 Jan
+          1982</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'ArialMT', color: '#ff0000' }}>
+          </span><br /><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '12pt', fontFamily: 'ArialMT', color: '#000000' }}>ID NO:
+          </span><span style={{ fontStyle: 'normal', fontWeight: 'bold', fontSize: '12pt', fontFamily: 'ArialMT', color: '#ff0000' }}>&nbsp; 6859161942</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '9pt', fontFamily: 'ArialMT', color: '#ff0000' }}>
+          </span><br /></div>
+        <img style={{ position: 'absolute', top: '1.55in', left: '0.84in', width: '3.66in', height: '0.02in' }} src={v5} />
+        <img style={{ position: 'absolute', top: '0.83in', left: '4.60in', width: '3.67in', height: '0.02in' }} src={v6} />
+        <img style={{ position: 'absolute', top: '0.83in', left: '8.26in', width: '0.02in', height: '2.33in' }} src={v7} />
+        <img style={{ position: 'absolute', top: '3.15in', left: '4.60in', width: '3.67in', height: '0.02in' }} src={v8} />
+        <img style={{ position: 'absolute', top: '0.83in', left: '4.60in', width: '0.02in', height: '2.33in' }} src={v9} />
+        <div style={{ position: 'absolute', top: '0.88in', left: '4.72in', width: '3.45in', lineHeight: '0.15in' }}><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#000000' }}>এই কার্ডটি
+          গণপ্রজাতন্ত্রী বাংলােদশ সরকারের সম্পত্তি। </span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#000000' }}> কার্ডটি
+            ব্যবহারকারী ব্যতীত অন্য</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#000000' }}>
+          </span><br /><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#000000' }}>কোথাও পাওয়া গেলে
+            নিকটস্থ পোষ্ট অফিসে জমা দেবার জন্য অনুরোধ করা হলো। </span><br /></div>
+        <div style={{ position: 'absolute', top: '1.04in', left: '4.72in', width: '3.45in', lineHeight: '0.15in' }}>
+        </div>
+        <div style={{ position: 'absolute', top: '1.26in', left: '4.71in', width: '3.43in', lineHeight: '0.15in' }}>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '9pt', fontFamily: 'Bangla', color: '#000000' }}>ঠিকানা:
+            বাসা/হোল্ডিং: চান মিয়ার বাড়ি, গ্রাম/রাস্তা: আখিতারা, ডাকঘর: নোয়াগাঁও - ৩৪৩০, সরাইল, ব্রাহ্মণবাড়িয়া
+          </span></div>
+        <div style={{ position: 'absolute', top: '1.80in', left: '4.68in', width: '0.50in', lineHeight: '0.15in' }}>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#000000' }}>রক্তের
+            গ্রুপ </span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '9pt', fontFamily: 'Bangla', color: '#000000' }}>
+          </span><br /></div>
+        <div style={{ position: 'absolute', top: '1.80in', left: '5.08in', lineHeight: '0.15in' }}>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'ArialMT', color: '#000000' }}> &nbsp;/ </span>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'ArialMT', color: '#000000' }}>Blood Group: <span style={{ color: "red", fontWeight: 'blod' }}>A+</span></span><br /></div>
+        <div style={{ position: 'absolute', top: '1.80in', left: '6.40in', width: '0.84in', lineHeight: '0.14in' }}>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#000000' }}>জন্মস্থান:
+            ব্রাহ্মণবাড়িয়া</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#000000' }}>
+          </span><br /></div>
+        <img style={{ position: 'absolute', top: '1.87in', left: '7.91in', width: '0.35in', height: '0.13in' }} src={v10} />
+        <div style={{ position: 'absolute', top: '1.84in', left: '7.91in', width: '0.39in', lineHeight: '0.14in' }}>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#ffffff' }}>মূদ্রণ:
+            ০১</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '8pt', fontFamily: 'Bangla', color: '#ffffff' }}>
+          </span><br /></div>
+        <img style={{ position: 'absolute', top: '2.05in', left: '4.92in', width: '0.88in', height: '0.33in' }} src={r5} />
+        <div style={{ position: 'absolute', top: '2.35in', left: '4.71in', width: '1.41in', lineHeight: '0.18in' }}>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>প্রদানকারী
+            কতৃপক্ষের স্বাক্ষর</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '10pt', fontFamily: 'Bangla', color: '#000000' }}>
+          </span><br /></div>
+        <div style={{ position: 'absolute', top: '2.36in', left: '6.69in', width: '1.37in', lineHeight: '0.15in' }}>
+          <span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '9pt', fontFamily: 'Bangla', color: '#000000' }}>প্রদানের তারিখ:
+            ০২/১১/২০২১</span><span style={{ fontStyle: 'normal', fontWeight: 'normal', fontSize: '9pt', fontFamily: 'Bangla', color: '#000000' }}>
+          </span><br /></div>
+        <img style={{ position: 'absolute', top: '2.61in', left: '4.71in', width: '3.51in', height: '0.44in' }} src={r6} />
+        <img style={{ position: 'absolute', top: '1.23in', left: '4.61in', width: '3.66in', height: '0.02in' }} src={v11} />
+        <img style={{ position: 'absolute', top: '2.00in', left: '4.61in', width: '3.66in', height: '0.02in' }} src={v12} />
+      </div>
+
+
+    </div >
   );
 }
 
